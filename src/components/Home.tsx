6af06d82f1399aec8685650c41ba181cd927e330
Anissa3005 <anissa.chadouli@gmail.com>
2023-07-19 11:18:20 +0900

    feat: making a side menu for mobile

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
-import "./Home.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import "./Home.css";
+
 
 function Home() {
     const [isLogin, setIsLogin] = useState<boolean>(true)
+    const [hamburger, setHamburger] = useState<boolean>(true)
     return (
         <div>
             <header>
                 <h2 className="header-title">Omamori Finder</h2>
-                <nav>
-                    <ul className="nav-list">
-                        {isLogin ? 
-                            (<>
-                                <li className="login-li">Username</li>
-                            </>)
-                        : <li className="not-login-li">login</li>
-                        }
-                        
-                    </ul>
-                </nav>
+                {hamburger ? (
+                    <FontAwesomeIcon onClick={() => setHamburger(!hamburger)} className="hamburger-menu" icon={faBars} />
+                ) : 
+                (   
+                    <div className="side-menu">
+                        <div className="xmark-container">
+                            <FontAwesomeIcon className="xmark"  onClick={() => setHamburger(!hamburger)} icon={faXmark} /> 
+                        </div>
+                        <ul className="nav-list">
+                            <li className="menu-list">Login</li>
+                            <li className="menu-list">About</li>
+                        </ul>
+                    </div> 
+                )
+                }
             </header>
         </div>
     )
