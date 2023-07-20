ffc748111dc7548dfc5026a7aeed4c4b8a80eb59
Anissa3005 <anissa.chadouli@gmail.com>
2023-07-20 22:08:41 +0900

    feat: figuring out how to overlay over react leaflet map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,8 +5,8 @@ import { MapContainer, TileLayer } from "react-leaflet";
 
 function Map() {
     return (
-        <div>
-            <MapContainer center={[35.652832, 139.839478]} zoom={10}>
+        <div className="master-container">
+            <MapContainer className="map" center={[35.652832, 139.839478]} zoom={8} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
