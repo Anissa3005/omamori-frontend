bf5b88b6bf119dba93e8693212993af896ef10a5
Anissa3005 <anissa.chadouli@gmail.com>
2023-08-09 14:25:02 +0900

    feat: created user context

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import logo from './logo.svg';
 import Launch from './components/Launch';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import RoutesComponent from './Routes';
+import {UserContext, UserContextProvider}  from './context/UserContext';
+import Login from './components/Login';
 
 function App() {
   return (
-    <>
-        <RoutesComponent />
-    </>
+   
+    <UserContextProvider>
+      <RoutesComponent />
+    </UserContextProvider>
+      
+   
   )
 }
 
--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,10 +1,10 @@
-import { createContext } from "react";
+import { Dispatch, FC, ReactNode, SetStateAction, createContext, useState } from "react";
 
 interface UserContextType {
     userName: string | null,
-    setUserName: (userName: string | null) => void,
+    setUserName: Dispatch<SetStateAction<string | null>>,
     userId: number | null,
-    setUserId: (userId: number | string) => void
+    setUserId: Dispatch<SetStateAction<number | null>>
 }
 
 const UserContext = createContext<UserContextType>({
@@ -12,4 +12,23 @@ const UserContext = createContext<UserContextType>({
     setUserName: () => {},
     userId: null,
     setUserId: () => {}
-})
+});
+
+interface Props {
+    children: ReactNode,
+    initialName?: string | null,
+    initialId?: number | null
+}
+
+const UserContextProvider: FC<Props> = ({children, initialName = null, initialId = null}) => {
+    const [userName, setUserName] = useState<string | null>(initialName)
+    const [userId, setUserId] = useState<number | null>(initialId)
+
+    return (
+        <UserContext.Provider value={{userName, setUserName, userId, setUserId}}>
+            {children}
+        </UserContext.Provider>
+        )
+}
+
+export  {UserContextProvider, UserContext}
