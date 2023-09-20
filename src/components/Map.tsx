ec085e290bb0090c7897dd335de8f0e7478a1b95
Anissa3005 <anissa.chadouli@gmail.com>
2023-09-20 14:06:05 +0900

    feat: add pin on map
    feat: previously the pin would not show up, I added a stylesheet to html to make it work

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,39 +1,49 @@
-import "leaflet/dist/leaflet.css";
-import "./Map.css";
-import "leaflet-geosearch/dist/geosearch.css";
+import "leaflet/dist/leaflet.css"
+import "./Map.css"
+import "leaflet-geosearch/dist/geosearch.css"
 import pin from "../assets/pin.png"
-// import "react-leaflet-geosearch/lib/react-leaflet-geosearch.css";
-import { useEffect } from "react";
-import { Icon } from "leaflet";
-import L from "leaflet"
-import { MapContainer, TileLayer, useMap} from "react-leaflet";
-import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
+import { useEffect, useState } from "react";
+import { Icon, map, } from "leaflet"
+import { MapContainer, TileLayer, useMap} from "react-leaflet"
+import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch"
 
 const customIcon = new Icon({
     iconUrl: pin,
-
     iconSize: [38, 38],
-  });
-
-const SearchControl = (props: any) => {
-    console.log(props)
-    const map = useMap();
-  
-    useEffect((): any => {
-      const searchControl = GeoSearchControl({
-        provider: new OpenStreetMapProvider(),
-        showMarker: true,
-        marker: customIcon
-      });
-  
-      map.addControl(searchControl);
-      return () => map.removeControl(searchControl);
-    }, [props]);
-  
-    return null;
-}
+  })
 
 function Map() {
+    const [langLong, setLangLong] = useState<[number, number]>([0,0])
+
+    const SearchControl = (props: any) => {
+        const map = useMap();
+      
+        useEffect((): any => {
+          const searchControl = GeoSearchControl({
+            style: "bar",
+            provider: new OpenStreetMapProvider(),
+            marker: {
+                icon: customIcon
+            },
+            popupFormat: (result: any) => {
+                return <p>{result.label}</p>
+            }
+        })
+    
+        map.on("geosearch/showlocation", (result: any) => {
+        let lang: number = result.location.y
+        let long: number = result.location.x
+        setLangLong([lang, long])
+        })
+
+        map.addControl(searchControl);
+        return () => map.removeControl(searchControl)
+    
+        }, [props]);
+      
+        return null;
+    }
+
     return (
         <div className="master-container">
             <MapContainer className="map" center={[35.652832, 139.839478]} zoom={8} scrollWheelZoom={false}>
@@ -42,15 +52,15 @@ function Map() {
                     url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 <SearchControl
-                    showMarker={true}
-                    showPopup={false}
-                    popupFormat={(result: any) => result.label}
-                    maxMarkers={3}
-                    retainZoomLevel={false}
-                    animateZoom={true}
-                    autoClose={false}
-                    searchLabel={"Enter address, please"}
-                    keepResult={true}
+                    // showMarker={true}
+                    // showPopup={false}
+                    // popupFormat={(result: any) => result.label}
+                    // maxMarkers={3}
+                    // retainZoomLevel={false}
+                    // animateZoom={true}
+                    // autoClose={false}
+                    // searchLabel={"Enter address, please"}
+                    // keepResult={true}
                 />
             </MapContainer>
         </div>
