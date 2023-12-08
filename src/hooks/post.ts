5a1755678d382fc565bd81b187f5d1dc46105fb6
Anissa3005 <anissa.chadouli@gmail.com>
2023-12-08 18:06:38 +0900

    change: cleanup the signup page

--- src/hooks/post.ts
+++ src/hooks/post.ts
@@ -1,44 +1,30 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { FC } from "react";
 
-interface User {
-    username: string,
-    email: string
+type User = {
+    uuid: string
 }
 
 interface Email {
     email?: string | null
 }
 
-export const usePostUser = (body: User) => {
-    const {data, isLoading} = useMutation({
-        mutationKey: ['user'],
-        mutationFn: async() => {
-            const {data} = await axios.post('http://127.0.0.1:8000/user/', body)
-            return data
-        }
-    })
-
-    return {data, isLoading}
-}
+// const usePostUser = (body: User) => {
+//     return useMutation({
+//         mutationKey: ['user'],
+//         mutationFn: async() => {
+//             const {data} = await axios.post('http://127.0.0.1:8000/user/', body)
+//             return data
+//         }
+//     })
+// }
 
 export const UseGetUser = (username: string) => {
-    const url = `http://127.0.0.1:8000/user/${username}/`
     return useQuery({
         queryKey: ['getuser', username],
         enabled: false,
         queryFn: async() => {
             const {data} = await axios.get(`http://127.0.0.1:8000/user/${username}/`)
-            return data
-        }
-    })
-}
-
-export const useGetUserByMail = () => {
-    return useMutation({
-        mutationFn: async(email: Email) => {
-            const {data} = await axios.post('http://127.0.0.1:8000/user/email/', email)
             return data
         }
     })
